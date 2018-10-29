5cf1c9756cfc67d05bf427389ccf48f4b12010e4
Emil Engler <emilstud2015@gmail.com>
2018-10-29 17:38:42 +0100

    Improve list to select a package manager

--- src/main.cpp
+++ src/main.cpp
@@ -9,26 +9,18 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	vector<string> package_manager_list = { "apt-get", "xbps", "dnf", "yum", "zypper", "eopkg", "pacman", "emerge", "pkg", "chromebrew", "homebrew", "nix", "snap", "npm" };
 	//Create a config file if the config file does not exists
 	if(!file_exists(CONFIG_PATH)) {
-		cout << "Please choose a package manager: " << endl << endl;
-		cout << "1. apt-get (Debian)" << endl;
-		cout << "2. xbps (Void)" << endl;
-		cout << "3. dnf (Fedora)" << endl;
-		cout << "4. yum (Enterprise Linux/Legacy Fedora)" << endl;
-		cout << "5. zypper (OpenSUSE)" << endl;
-		cout << "6. eopkg (Solus)" << endl;
-		cout << "7. pacman (Arch)" << endl;
-		cout << "8. emerge (Gentoo)" << endl;
-		cout << "9. pkg (FreeBSD)" << endl;
-		cout << "10. chromebrew (ChromeOS)" << endl;
-		cout << "11. homebrew (Mac OS)" << endl;
-		cout << "12. nix (Nix OS)" << endl;
-		cout << "13. snap (Independent)" << endl;
-		cout << "14. npm (Javascript, Global)" << endl << endl;
+		cout << "Please choose a package manager: " << endl;
+
+		for(int i = 0; i < package_manager_list.size(); i++) {
+			cout << (i+1) << ". " << package_manager_list[i] << endl;
+		}
+
+		cout << endl;
 
 		//Now lets listen for the input
-		vector<string> package_manager_list = { "apt-get", "xbps", "dnf", "yum", "zypper", "eopkg", "pacman", "emerge", "pkg", "chromebrew", "homebrew", "nix", "snap", "npm" };	//We use this to check if the input is valid
 		string input;
 		cin >> input;
 		//Convert the input to an int to see if it is valid
