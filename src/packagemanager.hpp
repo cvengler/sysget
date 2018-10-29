4495ca73d34dece1f7dc0116026ac69870b37d8f
Emil Engler <emilstud2015@gmail.com>
2018-10-29 16:11:47 +0100

    Improve code quality

--- src/packagemanager.hpp
+++ src/packagemanager.hpp
@@ -7,13 +7,6 @@ NOTE: Spaces are VERY important because we need them to append other strings to 
 */
 
 struct PackageManager {
-	string search;		//Search in the repos for a package
-	string install;		//Install a package
-	string remove;		//Remove a package
-	string autoremove;	//Remove orphans
-	string update;		//Update database
-	string upgrade;		//Upgrade all packages
-	string upgrade_pkg;	//Upgrade a specific package, if this isn't possible => reinstall
-	string clean;		//Clean the cache of the package manager
+	string search, install, remove, autoremove, update, upgrade, upgrade_pkg, clean;
 	void init(string pm);
 };
