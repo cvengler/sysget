9a7d816e9523fe4400428bebe858f30242dc8495
Emil Engler <emilstud2015@gmail.com>
2018-11-17 23:21:14 +0100

    Add autocompletion

---
+++ bash/bash-completion.bash
@@ -0,0 +1,11 @@
+_sysget()
+{
+	local cur prev opts
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	opts="search install remove update upgrade autoremove clean set help"
+
+	COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+}
+complete -F _sysget sysget
