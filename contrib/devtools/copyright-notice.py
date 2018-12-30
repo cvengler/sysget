50ac1ef63084e137bd89bad4dd195b1f0c6fbc93
Emil Engler <me@emilengler.com>
2018-12-30 22:09:08 +0100

    Fix copyright notice

--- contrib/devtools/copyright-notice.py
+++ contrib/devtools/copyright-notice.py
@@ -15,27 +15,33 @@ else:
     NOTICE = NOTICE.replace("<YEAR>" , "2018-" + str(now.year))
 
 
+NOTICE = NOTICE.split("\n")
+
 for ext in FILES:
     for filename in glob.iglob("../../src/" + ext, recursive=True):
         file = open(filename, "r")
         file_str = file.readlines()
         file.close()
         # Check if the first line matches the NOTICE
-        if file_str[1] != "// Distributed under the GNU GENERAL PUBLIC LICENSE Version 3, see the accompanying":
+        if file_str[0][0] != "/":
+            file_str.insert(0, NOTICE[0] + "\n")
+            file_str.insert(1, NOTICE[1] + "\n")
+            file_str.insert(2, NOTICE[2] + "\n")
             file = open(filename, "w")
-            [NOTICE] + file_str
             for x in file_str:
                 file.write(x)
 
             file.close()
+            print("Done with " + filename + " - NEW")
 
         else:
-            file_str.pop(0)
-            file_str.pop(1)
-            file_str.pop(2)
-            [NOTICE] + file_str
+            file_str = file_str[3:]
+            file_str.insert(0, NOTICE[0] + "\n")
+            file_str.insert(1, NOTICE[1] + "\n")
+            file_str.insert(2, NOTICE[2] + "\n")
             file = open(filename, "w")
             for x in file_str:
                 file.write(x)
 
             file.close()
+            print("Done with " + filename + " - UPDATE")
