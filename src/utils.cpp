5f41f74dff28d869f0df5a0d1fc31789e5574ec4
Emil Engler <me@emilengler.com>
2018-12-30 22:12:48 +0100

    Apply copyright notice

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2018 Emil Engler et al.
+// Distributed under the GNU GENERAL PUBLIC LICENSE Version 3, see the accompanying
+// file LICENSE.txt or <https://www.gnu.org/licenses/gpl-3.0.html>.
 #include "utils.hpp"
 
 bool file_exists(const char *filename) {
