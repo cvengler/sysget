acc860dee8d4c1485184e3d392e516d2d9649523
Emil Engler <me@emilengler.com>
2019-01-01 16:51:44 +0100

    Update copyright notice

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Emil Engler et al.
+// Copyright (c) 2018-2019 Emil Engler et al.
 // Distributed under the GNU GENERAL PUBLIC LICENSE Version 3, see the accompanying
 // file LICENSE.txt or <https://www.gnu.org/licenses/gpl-3.0.html>.
 #include "utils.hpp"
