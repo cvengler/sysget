d58f5944c37d213eb16dd22881ff34fa332e50c2
Emil Engler <me@emilengler.com>
2019-08-21 21:39:48 +0200

    Add LICENSE headers

--- src/main.hpp
+++ src/main.hpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2018-2019 Emil Engler et al.
+// Distributed under the GNU GENERAL PUBLIC LICENSE Version 3, see the accompanying
+// file LICENSE.txt or <https://www.gnu.org/licenses/gpl-3.0.html>.
 #ifndef MAIN_H
 #define MAIN_H
 #include <iostream>
