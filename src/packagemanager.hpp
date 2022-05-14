14ce2a8b7b2ba9193d697c8662b3e2ea7bf69531
Zachariah Cabelly <eternalblue34@duck.com>
2022-05-13 17:39:06 -0700

    add layman option

--- src/packagemanager.hpp
+++ src/packagemanager.hpp
@@ -12,7 +12,7 @@ NOTE: Spaces are VERY important because we need them to append other strings to 
 */
 
 struct PackageManager {
-	std::string search, install, uninstall, autoremove, update, upgrade, upgrade_pkg, clean;
+	std::string search, install, uninstall, autoremove, update, upgrade, upgrade_pkg, clean, layman;
 	void init(std::string pm);
 	void customPM(std::string filename);
 };
