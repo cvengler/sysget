c7f8e58b7a54598c2909d863f4964bdb6ee832f3
Zachariah Cabelly <eternalblue34@duck.com>
2023-04-27 09:57:27 -0700

    Make people run layman, although there is a sync plugin

--- src/packagemanager.cpp
+++ src/packagemanager.cpp
@@ -97,7 +97,6 @@ void PackageManager::init(std::string pm) {
 		uninstall = "emerge -C ";
 		autoremove = "emerge --depclean -a";
 		update = "emaint sync --all";
-		layman = "layman -f";
 		upgrade = "emerge --ask -u world && emerge --ask -uDN world";
 		upgrade_pkg = "emerge -avDuN";
 		clean = "eclean distfiles";
