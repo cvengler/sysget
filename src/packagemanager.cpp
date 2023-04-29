400bfa3bbbcc9233b626101bdbe140f750271224
td5038 <93895804+td5038@users.noreply.github.com>
2023-04-29 05:11:33 -0700

    Use native Portage tools by default (#46)

--- src/packagemanager.cpp
+++ src/packagemanager.cpp
@@ -96,7 +96,7 @@ void PackageManager::init(std::string pm) {
 		install = "emerge --ask ";
 		uninstall = "emerge -C ";
 		autoremove = "emerge --depclean -a";
-		update = "layman -f";
+		update = "emaint sync --all";
 		upgrade = "emerge --ask -u world && emerge --ask -uDN world";
 		upgrade_pkg = "emerge -avDuN";
 		clean = "eclean distfiles";
